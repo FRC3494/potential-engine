2f9157f6049c5fda7c7214d27b055e988616a868
Caleb Xavier Berger <caleb.x.berger@gmail.com>
2019-01-19 14:03:09 -0500

    Add options for different pipelines

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
 #include <string>
+#include <fmt/core.h>
+#include <getopt.h>
 #include <gst/gst.h>
 #include <gst/rtsp-server/rtsp-server.h>
+
+static int rpi_cam_flag = 0;
+static int use_hw_encoder = 0;
 
 int main(int argc, char *argv[]) {
     GMainLoop *loop;
@@ -12,15 +17,41 @@ int main(int argc, char *argv[]) {
     GSource *source;
     GError *err = NULL;
 
-    gst_init (&argc, &argv);
+    // pass no options to GStreamer so we can have our own
+    gst_init (NULL, NULL);
     loop = g_main_loop_new(NULL, false);
 
     server = gst_rtsp_server_new();
     mounts = gst_rtsp_server_get_mount_points(server);
     factory = gst_rtsp_media_factory_new();
-    // string = pipeline to use
-    // TODO: Use rpicamsrc if available
-    gst_rtsp_media_factory_set_launch(factory, "v4l2src ! video/x-raw,format=YUY2 ! videoconvert ! x264enc tune=zerolatency ! rtph264pay name=pay0");
+    // parse options
+    static struct option long_options[] = {
+        {"rpi_cam", no_argument, &rpi_cam_flag, 1},
+        {"no_rpi_cam", no_argument, &rpi_cam_flag, 0},
+        {"hardware_accel", no_argument, &use_hw_encoder, 1},
+        {0, 0, 0, 0}
+    };
+    while (1) {
+        int option_index = 0;
+        int c = getopt_long(argc, argv, "", long_options, &option_index);
+        if (c == -1) {
+            break;
+        }
+    }
+    std::string pipeline;
+    if (rpi_cam_flag == 1) {
+        pipeline = "rpicamsrc ! video/x-h264 ! h264parse ! rtph264pay name=pay0";
+    } else {
+        pipeline = "v4l2src ! video/x-raw,format=YUY2 ! videoconvert ! {} ! rtph264pay name=pay0";
+        if (use_hw_encoder == 0) {
+            // don't hardware accelerate
+            pipeline = fmt::format(pipeline, "x264enc tune=zerolatency");
+        } else {
+            pipeline = fmt::format(pipeline, "avenc_h264_omx");
+        }
+    }
+    std::cout << "Starting pipeline: " + pipeline << std::endl;
+    gst_rtsp_media_factory_set_launch(factory, pipeline.c_str());
     // g_signal_connect (factory, "media-configure", (GCallback) media_configure, NULL);
     // add stream at `/test`
     gst_rtsp_mount_points_add_factory(mounts, "/test", factory);
