00efe07de8b5052b2daedfe5339659a1c365fd38
Caleb Xavier Berger <caleb.x.berger@gmail.com>
2019-03-26 08:51:37 -0400

    Add getter + header file entry

--- src/optional.cpp
+++ src/optional.cpp
@@ -18,7 +18,7 @@ int rot = 0;
 bool prev = false;
 // V4L2 options
 bool hw_encoder = false;
-char *v4l2_device = (char *) "/dev/video0";
+char *v4l2_dev = (char *) "/dev/video0";
 // common options
 int height = DEFAULT_RESOLUTION;
 int fps = DEFAULT_FPS;
@@ -48,7 +48,7 @@ GOptionGroup *netOpts = g_option_group_new("net", "Networking options", "Show ne
 
 static GOptionEntry v4l2Entries[]{
         {"use_omx", 'o', G_OPTION_FLAG_NONE, G_OPTION_ARG_NONE,   &hw_encoder,  "Use OpenMAX hardware acceleration (default: false)", NULL},
-        {"device",  'd', G_OPTION_FLAG_NONE, G_OPTION_ARG_STRING, &v4l2_device, "Video4Linux2 device to use (default: /dev/video0)", "DEVICE"},
+        {"device",  'd', G_OPTION_FLAG_NONE, G_OPTION_ARG_STRING, &v4l2_dev, "Video4Linux2 device to use (default: /dev/video0)", "DEVICE"},
         {NULL}
 };
 GOptionGroup *v4l2Opts = g_option_group_new("v4l2", "Video4Linux2 options", "Show Video4Linux2 options", NULL, NULL);
@@ -74,6 +74,8 @@ int *rotation() { return &rot; }
 bool *preview() { return &prev; }
 
 bool use_hw_encoder() { return hw_encoder; }
+
+std::string v4l2_device() { return std::string(v4l2_dev); }
 
 int *video_height() { return &height; }
 
--- src/optional.h
+++ src/optional.h
@@ -3,23 +3,32 @@
 
 bool use_rpi_cam();
 
-int* rotation();
-bool* preview();
+int *rotation();
+
+bool *preview();
 
 bool use_hw_encoder();
 
-int* video_height();
-int* framerate();
+std::string v4l2_device();
 
-const char* address();
-int* port();
+int *video_height();
+
+int *framerate();
+
+const char *address();
+
+int *port();
+
 std::string mount();
 
 void init_options();
 
-GOptionEntry* get_main_opts();
-GOptionGroup* get_rpi_opts();
-GOptionGroup* get_v4l2_opts();
-GOptionGroup* get_net_opts();
+GOptionEntry *get_main_opts();
+
+GOptionGroup *get_rpi_opts();
+
+GOptionGroup *get_v4l2_opts();
+
+GOptionGroup *get_net_opts();
 
 bool judgemental();
